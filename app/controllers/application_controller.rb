7bb3cfe94af8d5dcde6fb90a1917e2f5d0bc1664
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-07-15 09:43:19 +0100

    extract data from token in application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,18 @@
 class ApplicationController < ActionController::API
-  acts_as_token_authentication_handler_for User, fallback: :none
+  # acts_as_token_authentication_handler_for User, fallback: :none
+
+  def current_user
+    @current_user || = User.find(payload['user_id'])
+  end
+
+  private
+
+  def payload 
+    JWT.decode get_token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256 '}
+  end
+
+  def get_token
+    request.headers['Authorization'].split(' ').last 
+  end
+
 end
