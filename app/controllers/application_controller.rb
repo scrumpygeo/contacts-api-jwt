8fa824b79ddac639d110a7db568102e2ec1f9044
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-07-15 13:09:30 +0100

    refactor jwt code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,20 +1,9 @@
 class ApplicationController < ActionController::API
   # acts_as_token_authentication_handler_for User, fallback: :none
 
-  def current_user
-    # binding.pry
-    @current_user ||= User.find(payload['user_id'])
-  end
-
-  private
-
-  def payload 
-    # payload gives us the user
-    JWT.decode(get_token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' }).first
-  end
-
-  def get_token
-    request.headers['Authorization'].split(' ').last 
-  end
+  # def current_user
+  #   # binding.pry
+  #   @current_user ||= User.find(payload['user_id'])
+  # end
 
 end
--- config/initializers/jwt_authenticatable.rb
+++ config/initializers/jwt_authenticatable.rb
@@ -5,7 +5,7 @@ module Devise
         token = get_token
         return fail(:invalid) unless token.present?
 
-        payload = get_payload
+        payload = WebToken.decode(token)
         return fail(:invalid) if payload == :expired
 
         resource = mapping.to.find(payload['user_id'])
@@ -17,12 +17,7 @@ module Devise
 
       private 
 
-      def get_payload 
-        JWT.decode(get_token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' }).first
-      rescue JWT::ExpiredSignature
-        :expired
-      end
-
+ 
       def get_token
         auth_header.present? && auth_header.split(' ').last 
       end
