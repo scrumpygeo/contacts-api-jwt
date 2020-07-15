7cf1ebeab0af8cab05d6d86de2e94fb424c75f00
scrumpygeo <scrumpygeo@yahoo.co.uk>
2020-07-15 10:40:42 +0100

    tidy up application_controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,13 +2,14 @@ class ApplicationController < ActionController::API
   # acts_as_token_authentication_handler_for User, fallback: :none
 
   def current_user
-    @current_user || = User.find(payload['user_id'])
+    # binding.pry
+    @current_user ||= User.find(payload['user_id'])
   end
 
   private
 
   def payload 
-    JWT.decode get_token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256 '}
+    JWT.decode(get_token, Rails.application.secrets.secret_key_base, true, { algorithm: 'HS256' }).first
   end
 
   def get_token
